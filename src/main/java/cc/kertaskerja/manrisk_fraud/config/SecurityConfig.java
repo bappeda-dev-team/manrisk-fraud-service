61fcfaa64475d555995728188ef26455d671d673
RyanB1303 <ryan.brilliant.nirwana@gmail.com>
2025-06-26 21:38:43 +0700

    disable cors hehe

--- src/main/java/cc/kertaskerja/manrisk_fraud/config/SecurityConfig.java
+++ src/main/java/cc/kertaskerja/manrisk_fraud/config/SecurityConfig.java
@@ -14,6 +14,7 @@ public class SecurityConfig {
     public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
         http
                 .csrf(csrf -> csrf.disable())
+                .cors(cors -> cors.disable())
                 .authorizeHttpRequests(authz -> authz
                         .requestMatchers("/api/**").permitAll()
                         .requestMatchers("/swagger-ui/**").permitAll()
