c4d25ae467f320ab4228e25f8613b37e0f680449
RyanB1303 <ryan.brilliant.nirwana@gmail.com>
2025-06-27 17:42:25 +0700

    config cors hehe

--- src/main/java/cc/kertaskerja/manrisk_fraud/config/SecurityConfig.java
+++ src/main/java/cc/kertaskerja/manrisk_fraud/config/SecurityConfig.java
@@ -35,7 +35,7 @@ public class SecurityConfig {
     public CorsConfigurationSource corsConfigurationSource() {
         CorsConfiguration configuration = new CorsConfiguration();
         configuration.setAllowedOrigins(List.of("http://localhost:3000")); // tambahkan origin yang diizinkan
-        configuration.setAllowedMethods(List.of("GET", "POST", "PUT", "DELETE", "OPTIONS"));
+        configuration.setAllowedMethods(List.of("GET", "POST", "PATCH", "PUT", "DELETE", "OPTIONS"));
         configuration.setAllowedHeaders(List.of("*"));
         configuration.setAllowCredentials(true); // jika menggunakan cookie/session
 
