e2f9475efdb16c604b7ff2454d809ffda1dc06b4
RyanB1303 <ryan.brilliant.nirwana@gmail.com>
2025-06-29 18:04:32 +0700

    update dari ilham

--- build/resources/main/db/migration/V6__create_jenis_risiko_table.sql
+++
@@ -1,7 +0,0 @@
-CREATE TABLE IF NOT EXISTS risiko_kecurangan (
-    id BIGSERIAL PRIMARY KEY,
-    jenis_risiko VARCHAR(255) NOT NULL,
-    uraian TEXT,
-    created_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
-    updated_at TIMESTAMP
-)
