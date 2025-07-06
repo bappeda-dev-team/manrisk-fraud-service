4a1dccf3acebbb1fd9ae6d32e3316fa16b841da2
Ryan B <ryan.brilliant.nirwana@gmail.com>
2025-07-06 19:24:09 +0700

    Update SecurityConfig.java
    add external to allowed

--- src/main/java/cc/kertaskerja/manrisk_fraud/config/SecurityConfig.java
+++ src/main/java/cc/kertaskerja/manrisk_fraud/config/SecurityConfig.java
@@ -30,7 +30,8 @@ public class SecurityConfig {
                                 "/v3/api-docs/**",
                                 "/swagger-resources/**",
                                 "/webjars/**",
-                                "/actuator/**"
+                                "/actuator/**",
+                                "/external/**"
                         ).permitAll()
 
                         // API public (opsional)
